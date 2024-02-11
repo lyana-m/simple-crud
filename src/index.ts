ab2515ec151b14c327dfc980ca8d281c38ff8fa0
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-02-11 16:09:33 +0500

    feat: implement dev mode

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello app');
+import http, {IncomingMessage, ServerResponse} from 'http';
+
+const PORT = 4000;
+
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  console.log(req, res)
+  console.log('Server request');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
